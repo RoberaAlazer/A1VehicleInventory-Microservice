7bfb06b11b6bc9ec65dc4143b07314267e28ca23
Robbie <ralazer3864@conestogac.on.ca>
2026-02-16 14:38:08 -0500

    add applcation repo interface and domain references

---
+++ VehicleInventory.Application/Interfaces/IVehicleRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleInventory.Domain.Entities;
+
+namespace VehicleInventory.Application.Interfaces
+{
+    public interface IVehicleRepository
+    {
+        Task<Vehicle> GetByIdAsync(int id);
+        Task<List<Vehicle>> GetAll();
+        Task<int> CreateAsync (Vehicle vehicle);
+
+        Task UpdateAsync(Vehicle vehicle);
+
+        Task DeleteAsync(int id);
+    }
+}
