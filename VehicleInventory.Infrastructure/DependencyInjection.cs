2f0eb36d9b8df36afaf42c6faba5d0d789e9b442
Robbie <ralazer3864@conestogac.on.ca>
2026-02-16 22:08:37 -0500

    wire infrastructure via DI extension

--- VehicleInventory.Infrastructure/DependencyInjection.cs
+++ VehicleInventory.Infrastructure/DependencyInjection.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VehicleInventory.Application.Interfaces;
+using VehicleInventory.Infrastructure.Data;
+using VehicleInventory.Infrastructure.Repositories;
 
-namespace VehicleInventory.Infrastructure
+namespace VehicleInventory.Infrastructure;
+
+public static class DependencyInjection
 {
-    internal class DependencyInjection
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<VehicleInventoryDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("VehicleInventoryDb")));
+
+        services.AddScoped<IVehicleRepository, VehicleRepository>();
+
+        return services;
     }
 }
--- VehicleInventory.WebAPI/Program.cs
+++ VehicleInventory.WebAPI/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using VehicleInventory.Infrastructure.Data;
+using VehicleInventory.Application.Services;
+using VehicleInventory.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,6 +13,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<VehicleService>();
+builder.Services.AddInfrastructure(builder.Configuration);
+
 
 var app = builder.Build();
 
