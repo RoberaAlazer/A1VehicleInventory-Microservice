fbe96cf8689720f528ec01f871815971a08cc2a1
Robbie <ralazer3864@conestogac.on.ca>
2026-03-16 17:34:21 -0400

    Add API Gateway reverse proxy API key middleware

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,6 +1,9 @@
+using ApiGateway.Middleware;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
+app.UseMiddleware<ApiKeyMiddleware>();
+app.MapReverseProxy();
 
 app.Run();
