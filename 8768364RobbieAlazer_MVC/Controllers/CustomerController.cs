acdf769d64bf4860dd66ff6e1ea7f90c382cd42c
RoberaAlazer <roberaalazer@gmail.com>
2026-04-13 17:26:42 -0400

    A3: complete step 11 testing and gateway integration

---
+++ 8768364RobbieAlazer_MVC/Controllers/CustomerController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Json;
+
+namespace _8768364RobbieAlazer_MVC.Controllers;
+
+public class CustomersController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public CustomersController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var client = _httpClientFactory.CreateClient("GatewayClient");
+        var customers = await client.GetFromJsonAsync<List<CustomerDto>>("gateway/customers");
+        return View(customers ?? new List<CustomerDto>());
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Create(CustomerDto dto)
+    {
+        var client = _httpClientFactory.CreateClient("GatewayClient");
+        var res = await client.PostAsJsonAsync("gateway/customers", dto);
+        if (!res.IsSuccessStatusCode) TempData["Err"] = await res.Content.ReadAsStringAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var client = _httpClientFactory.CreateClient("GatewayClient");
+        var res = await client.DeleteAsync($"gateway/customers/{id}");
+        if (!res.IsSuccessStatusCode) TempData["Err"] = await res.Content.ReadAsStringAsync();
+        return RedirectToAction(nameof(Index));
+    }
+}
+
+public record CustomerDto(int Id, string FirstName, string LastName, string Phone, string Email);
