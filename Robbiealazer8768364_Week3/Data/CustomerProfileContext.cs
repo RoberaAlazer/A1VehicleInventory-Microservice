aa5ecf20ff63a5ef1aa4cb8ecb571eaf345a7671
RoberaAlazer <roberaalazer@gmail.com>
2026-04-13 21:08:58 -0400

    Docker

--- Robbiealazer8768364_Week3/Data/CustomerProfileContext.cs
+++ Robbiealazer8768364_Week3/Data/CustomerProfileContext.cs
@@ -18,10 +18,6 @@ public partial class CustomerProfileContext : DbContext
 
     public virtual DbSet<Customer> Customers { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=CustomersProfile;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Customer>(entity =>
